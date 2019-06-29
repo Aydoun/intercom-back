fc88d7d7c1486d68b5368065a3959a1df7e321ee
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2019-06-29 13:29:40 +0300

    feat(Conversation): add conversation model

---
+++ src/models/conversation.model.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const Messages = new Schema({
+  messageId : Schema.Types.ObjectId,
+  content : {type : String, requires: true},
+  sender: Schema.Types.ObjectId,
+  status : {type : String , enum:['read' , 'unread'] , default : 'unread'}
+} , {timestamps: true});
+
+const Conversation = new Schema({
+  creator : {type : Schema.Types.ObjectId, required: true},
+  receiver: {type : Schema.Types.ObjectId, required: true},
+  participatants: [Schema.Types.ObjectId],
+  messages: [Messages],
+} , {timestamps: true});
+
+module.exports = mongoose.model('conversation', Conversation);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,30 +3,26 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const UserSchema = Schema({
-  name: {
-    type: String,
-    required: true,
-    match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
-  },
+  name: { type: String, required: true },
   sexe: { type: Number, enum: [0, 1, -1], default: 0 },
-  addresses: { type: Array },
-  contactInfo: { type: Array },
-  phone: { type: String },
+  addresses: [String],
+  contactInfo: [String],
+  phone: [String],
   profession: { type: String },
-  bio: { type: String },
-  privacy: { type: Array },
+  bio: { type: String, default: '' },
+  privacy: { type: Array, default: [] },
   avatar: { type: String, default: '' },
   email: {
     type: String,
     required: true,
     lowercase: true,
-    match: [/\S+@\S+\.\S+/, 'is invalid'],
     index: { unique: true },
   },
   password: {
     type: String,
     required: true,
   },
+  conversations: [Schema.Types.ObjectId],
   status: { type: String, enum: ['Active', 'Inactive', 'Banned'], default: 'Active' },
 }, { timestamps: true });
 
