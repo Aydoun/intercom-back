1118ffc02aad1872b0ee0dc070b5b053b31f5026
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2019-07-06 11:35:55 +0200

    feat(Repository): add completion status to endpoints

--- src/routes/repository.js
+++ src/routes/repository.js
@@ -1,68 +1,58 @@
 import express from 'express';
-import { param, query, body} from 'express-validator';
-import {
-    fetchHistory,
-    listStatus,
-    listTree,
-    submitCommit,
-    createBranch,
-    getBranch,
-    removeBranch,
-    mergeBranch,
-    getSummary,
-} from 'controllers/repository.controller';
+import { param, query, body } from 'express-validator';
+import * as R from 'controllers/repository.controller';
 import { catchValidationError } from 'utils/validation';
 
 const repository = express.Router();
 
 repository.use([
-    '/:repoName/history',
-    '/:repoName/commit',
+    '/:repoName/history', // Done
+    '/:repoName/commit', // Done
     '/:repoName/file',
-    '/:repoName/tree',
+    '/:repoName/tree', // Done
     '/:repoName/participants',
-    '/:repoName/status',
-    '/:repoName/merge',
-    '/:repoName/file/history',
-    '/:repoName/commit/:sha/diff',
+    '/:repoName/status', // Done
+    '/:repoName/merge', // Done
+    '/:repoName/file/history', // Postponed
+    '/:repoName/commit/:sha/diff', // Postponed
 ], [
     param('repoName').isUUID(),
     query('branch').exists(),
 ], catchValidationError);
 
-repository.get('/:repoName/history', fetchHistory);
-repository.get('/:repoName/status', listStatus);
-repository.get('/:repoName/tree', listTree);
+repository.get('/:repoName/history', R.fetchHistory);
+repository.get('/:repoName/status', R.listStatus);
+repository.get('/:repoName/tree', R.listTree);
 
 repository.post('/:repoName/commit', [
     body('username').exists(),
     body('email').isEmail(),
     body('message').exists(),
-], catchValidationError, submitCommit);
+], catchValidationError, R.submitCommit);
 
 repository.get('/:repoName/branch', [
     param('repoName').isUUID(),
-], catchValidationError, getBranch);
+], catchValidationError, R.getBranch);
 
 repository.post('/:repoName/branch', [
     param('repoName').isUUID(),
     query('branch').exists(),
     query('branchName').exists(),
-], catchValidationError, createBranch);
+], catchValidationError, R.createBranch);
 
 repository.delete('/:repoName/branch', [
     param('repoName').isUUID(),
     query('branch').exists(),
-], catchValidationError, removeBranch);
+], catchValidationError, R.removeBranch);
 
 repository.post('/:repoName/merge', [
     body('username').exists(),
     body('email').isEmail(),
-], catchValidationError, mergeBranch);
+], catchValidationError, R.mergeBranch);
 
 repository.get('/:repoName/summary', [
     param('repoName').isUUID(),
     query('repoId').exists(),
-], catchValidationError, getSummary);
+], catchValidationError, R.getSummary);
 
 module.exports = repository;
