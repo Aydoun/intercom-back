734f812a446faea6a933211e1ba33585935942d2
Mohamed Aydoun <mohamed.aydoun@tele2.com>
2019-09-06 14:50:38 +0200

    feat(Plans): get plans intersections

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,17 +1,18 @@
 import express from 'express';
-import * as U from 'controllers/user.controller';
+import * as C from 'controllers/user.controller';
 import { check, query, body } from 'express-validator';
 import { catchValidationError } from 'utils/validation';
 
 const users = express.Router();
 
-users.get('', U.userDetails);
-users.get('/plans', U.userPlansList);
-users.get('/search', query('name').exists(), catchValidationError, U.searchByname);
-users.get('/activity', U.listActivity);
-users.get('/:id', check('id').isMongoId(), catchValidationError, U.getUserById);
-users.post('/activity', body('actionType').exists(), catchValidationError, U.addActivity);
-users.put('', U.update);
-users.delete('', U.remove);
+users.get('', C.userDetails);
+users.get('/plans', C.userPlansList);
+users.get('/search', query('name').exists(), catchValidationError, C.searchByname);
+users.get('/activity', C.listActivity);
+users.get('/intersection', query('userId').isMongoId(), catchValidationError, C.planIntersection);
+users.get('/:id', check('id').isMongoId(), catchValidationError, C.getUserById);
+users.post('/activity', body('actionType').exists(), catchValidationError, C.addActivity);
+users.put('', C.update);
+users.delete('', C.remove);
 
 module.exports = users;
--- src/services/user/user.service.js
+++ src/services/user/user.service.js
@@ -12,3 +12,4 @@ export const searchUser = U.searchUser;
 
 export const saveActivity = U.saveActivity;
 export const getActivity = U.getActivity;
+export const getIntersection = U.getIntersection;
