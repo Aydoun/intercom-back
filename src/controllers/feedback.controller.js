1f83be2f7452e4f9a99b9bc985e43384471d7115
Mohame Aydoun <mohamed.aydoun@frontmen.nl>
2019-11-10 12:46:54 +0100

    feat(Feedback): persist feedback activity

--- src/controllers/feedback.controller.js
+++ src/controllers/feedback.controller.js
@@ -1,4 +1,6 @@
 import FeedbackModel from 'models/feedback.model';
+import { pushActivity } from 'services/activity/activity.service';
+import { ActivityPoint, ActivityType } from 'constants';
 
 export const saveFeedback = (req, res) => {
   const { id } = req.tokenData;
@@ -6,8 +8,14 @@ export const saveFeedback = (req, res) => {
 
   const newFeedback = new FeedbackModel({ message, creator: id, score });
 
-  newFeedback.save(newFeedback)
+  return newFeedback.save(newFeedback)
   .then(() => res.formatResponse({}))
+  .then(() => {
+    const pointValue = ActivityPoint.feedback;
+    const typeValue = ActivityType.feedback;
+
+    return pushActivity(id, pointValue, typeValue);
+  })
   .catch(err => res.formatResponse(err.message, 401));
 };
 
